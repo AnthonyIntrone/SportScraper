d08e37337e65f7af03e49828e629e8f0f25f797d
AnthonyIntrone <theintrone@gmail.com>
2019-04-11 15:30:45 -0400

    Was able to get the text

--- Lab2/NYTArticles.py
+++ Lab2/NYTArticles.py
@@ -1,20 +1,41 @@
 import json
 import requests
+from bs4 import BeautifulSoup
 
 
-
-def parse_articles():
+# *********************************************************************** E-Sports CODE ***********************************************************************
+def grab_urls():
     urls = []
     for i in range(0,10):
-        
-        api_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=nba&offset=" + str(i) + "&api-key=9vBbIBydobSjA6kGoUqq0MNGg4rqZtLy"
+        api_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=e-sports&offset=" + str(i) + "&api-key=9vBbIBydobSjA6kGoUqq0MNGg4rqZtLy"
         data = requests.get(api_url)
         data = data.json()
         data = data['response']['docs']
         for url in data:
             urls.append(url['web_url'])
-
-    print(urls)
     return urls
 
-league_articles = parse_articles()
+
+def grab_text(url):
+    
+    session = requests.Session()
+    req = session.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    paragraphs = soup.find_all('p')
+
+    article = ""
+    for p in paragraphs:
+        article = article + p.get_text()
+    return article
+
+def process_data():
+    league_articles = grab_urls()
+    league_articles_text = ""
+    for article in league_articles:
+        league_articles_text = league_articles_text + grab_text(article)
+
+    print(league_articles_text)
+    
+process_data()
+
+# *********************************************************************** NHL Code ***********************************************************************
